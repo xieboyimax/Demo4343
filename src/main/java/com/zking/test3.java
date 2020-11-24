cd5b80a66532255c2b5f905aa9061864c55f921a
xieboyimax <xieboyimax@qq.com>
2020-11-24 09:51:48 +0800

    测试

--- src/main/java/com/zking/test3.java
+++ src/main/java/com/zking/test3.java
@@ -1,7 +1,7 @@
 package com.zking;
 
-public class test2 {
-    public static void main(String[] args) {
-      System.out.println("这是来自于GitHub的问候！");
+public class test3 {
+    public static void main(String[] args){
+        System.out.println("测试");
     }
-}    
+}
